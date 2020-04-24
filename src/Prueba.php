ecc5216ad46d080cf3adb6bbb6a3751a16cb5e75
Carlos Quintero <info.fxstudios@gmail.com>
2020-04-24 17:15:40 -0500

    Inicio

---
+++ src/Prueba.php
@@ -0,0 +1,32 @@
+<?php
+namespace Xfxstudios\Miclase;
+
+class Prueba{
+
+    private $name;
+    private $apellido;
+    private $bienvenida;
+
+    public function setName($name){
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setApellido($x){
+        $this->apellido = $x;
+        return $this;
+    }
+
+    public function setBienvenida($x=null){
+        $this->bienvenida = ($x==null) ? "Bienvenido " : $x;
+        return $this;
+    }
+
+    public function getSaludo(){
+        return $this->bienvenida.' '.$this->name.' '.$this->apellido;
+    }
+
+}
+
+
+?>
